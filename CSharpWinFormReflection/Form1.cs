d71b20cfaa2703eeb17ef9e21525f6662e93f7bb
King Liu <ntutking@hotmail.com>
2020-10-14 09:35:49 +0800

    Add a URL to Reference

--- CSharpWinFormReflection/Form1.cs
+++ CSharpWinFormReflection/Form1.cs
@@ -4,10 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// https://www.itread01.com/content/1552328408.html
 namespace CSharpWinFormReflection
 {
     public partial class Form1 : Form
@@ -15,6 +16,13 @@ namespace CSharpWinFormReflection
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var file = @"D:\Positions\RobotPathGetter\MvAssistant.Mac.VerifyEqp.exe";
+            Assembly assemFrom =Assembly.LoadFrom(file);
+            Assembly assemFile = Assembly.LoadFile(file);
         }
     }
 }
